508d4b3a6886233b08c3b67a813578182363323d
WinterBear <winterbear.mail@gmail.com>
2017-02-11 16:15:19 +0000

    Modify App Theme Style to inherit from `android:Theme.Material`

--- app/src/main/java/io/github/drlladd/smstext/MainActivity.java
+++ app/src/main/java/io/github/drlladd/smstext/MainActivity.java
@@ -1,9 +1,10 @@
 package io.github.drlladd.smstext;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
